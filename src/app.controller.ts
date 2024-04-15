fa0bc7c2d1b63283b101939b92505625df40eb68
Gabriel-souzaa <gabrielfreitassouzaa@gmail.com>
2024-04-15 15:39:27 -0300

    feat: to add interceptor pagination

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ErrorGenericException } from './errors/exception';
+import { PaginationInterceptor } from './interceptors/pagination.interceptor';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    throw new ErrorGenericException('ZOD_VALIDATION');
-
+  @UseInterceptors(PaginationInterceptor)
+  getHello(): string[] {
     return this.appService.getHello();
+  }
+
+  @Get('/err')
+  getError(): string[] {
+    throw new ErrorGenericException('ZOD_VALIDATION');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MyLogger } from './logger/logger';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { ErrorHandlerFilter } from './errors/filter';
+import { PaginationInterceptor } from './interceptors/pagination.interceptor';
 
 @Global()
 @Module({
@@ -15,6 +16,10 @@ import { ErrorHandlerFilter } from './errors/filter';
     {
       provide: APP_FILTER,
       useClass: ErrorHandlerFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: PaginationInterceptor,
     },
   ],
   exports: [MyLogger],
